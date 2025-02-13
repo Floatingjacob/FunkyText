1dde317bbc3d02caedcdcb2249941309d8c7083b
Jacob <108031526+Floatingjacob@users.noreply.github.com>
2025-02-13 16:45:12 -0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form replaces Dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.cmbMode = New System.Windows.Forms.ComboBox()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnCopy = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(12, 12)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(260, 20)
+        Me.txtInput.TabIndex = 0
+        '
+        'cmbMode
+        '
+        Me.cmbMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbMode.FormattingEnabled = True
+        Me.cmbMode.Items.AddRange(New Object() {"Leet Speak", "Symbolic", "Reverse"})
+        Me.cmbMode.Location = New System.Drawing.Point(12, 38)
+        Me.cmbMode.Name = "cmbMode"
+        Me.cmbMode.Size = New System.Drawing.Size(121, 21)
+        Me.cmbMode.TabIndex = 1
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Location = New System.Drawing.Point(12, 68)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(50, 13)
+        Me.lblOutput.TabIndex = 2
+        Me.lblOutput.Text = "Output..."
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(139, 36)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvert.TabIndex = 3
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'btnCopy
+        '
+        Me.btnCopy.Location = New System.Drawing.Point(220, 63)
+        Me.btnCopy.Name = "btnCopy"
+        Me.btnCopy.Size = New System.Drawing.Size(52, 23)
+        Me.btnCopy.TabIndex = 4
+        Me.btnCopy.Text = "Copy"
+        Me.btnCopy.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.ClientSize = New System.Drawing.Size(284, 100)
+        Me.Controls.Add(Me.btnCopy)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.cmbMode)
+        Me.Controls.Add(Me.txtInput)
+        Me.Name = "Form1"
+        Me.Text = "Funky Text"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents cmbMode As ComboBox
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents btnCopy As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,76 @@
+﻿Imports System.Text
+
+Public Class Form1
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        UpdateOutput()
+    End Sub
+
+    Private Sub txtInput_TextChanged(sender As Object, e As EventArgs) Handles txtInput.TextChanged
+        UpdateOutput()
+    End Sub
+
+    Private Sub cmbMode_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbMode.SelectedIndexChanged
+        UpdateOutput()
+    End Sub
+
+    Private Sub UpdateOutput()
+        Dim inputText = txtInput.Text
+        Dim selectedMode = cmbMode.SelectedItem?.ToString()
+
+        If String.IsNullOrEmpty(inputText) OrElse String.IsNullOrEmpty(selectedMode) Then
+            lblOutput.Text = "Please enter text and select a mode."
+            Return
+        End If
+
+        Select Case selectedMode
+            Case "Leet Speak"
+                lblOutput.Text = ConvertLeet(inputText)
+            Case "Symbolic"
+                lblOutput.Text = ConvertSymbolic(inputText)
+            Case "Reverse"
+                lblOutput.Text = StrReverse(inputText)
+            Case Else
+                lblOutput.Text = inputText
+        End Select
+    End Sub
+
+    Private Function ConvertLeet(input As String) As String
+        Dim dict As New Dictionary(Of Char, String) From {
+            {"a"c, "4"}, {"b"c, "B"}, {"c"c, "C"}, {"d"c, "D"}, {"e"c, "3"},
+            {"f"c, "F"}, {"g"c, "G"}, {"h"c, "H"}, {"i"c, "1"}, {"j"c, "J"},
+            {"k"c, "K"}, {"l"c, "L"}, {"m"c, "M"}, {"n"c, "N"}, {"o"c, "0"},
+            {"p"c, "P"}, {"q"c, "Q"}, {"r"c, "R"}, {"s"c, "$"}, {"t"c, "7"},
+            {"u"c, "U"}, {"v"c, "V"}, {"w"c, "W"}, {"x"c, "X"}, {"y"c, "Y"},
+            {"z"c, "Z"}
+        }
+
+        Return TransformText(input, dict)
+    End Function
+
+    Private Function ConvertSymbolic(input As String) As String
+        Dim dict As New Dictionary(Of Char, String) From {
+            {"a"c, "@"}, {"b"c, "ß"}, {"c"c, "¢"}, {"d"c, "Ð"}, {"e"c, "£"},
+            {"f"c, "ƒ"}, {"g"c, "Ǥ"}, {"h"c, "Ħ"}, {"i"c, "¡"}, {"j"c, "J"},
+            {"k"c, "Ҝ"}, {"l"c, "Ł"}, {"m"c, "M"}, {"n"c, "Ñ"}, {"o"c, "°"},
+            {"p"c, "Þ"}, {"q"c, "Q"}, {"r"c, "®"}, {"s"c, "§"}, {"t"c, "†"},
+            {"u"c, "µ"}, {"v"c, "V"}, {"w"c, "₩"}, {"x"c, "×"}, {"y"c, "¥"},
+            {"z"c, "Ƶ"}, {" "c, "  "}
+        }
+
+        Return TransformText(input, dict)
+    End Function
+
+    Private Function TransformText(input As String, dict As Dictionary(Of Char, String)) As String
+        Dim result As New StringBuilder()
+        For Each c In input
+            Dim lowerChar = Char.ToLower(c)
+            result.Append(If(dict.ContainsKey(lowerChar), dict(lowerChar), c))
+        Next
+        Return result.ToString()
+    End Function
+
+    ' Copy to clipboard
+    Private Sub btnCopy_Click(sender As Object, e As EventArgs) Handles btnCopy.Click
+        Clipboard.SetText(lblOutput.Text)
+    End Sub
+End Class
